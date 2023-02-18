a0d7651bb0f07f4de655e852e940c9c08ae21bfc
Rohit-gi <i.rohit003@gmail.com>
2023-02-18 15:37:25 +0530

    Updates

---
+++ precache-manifest.a78145a25905c9853b290fca487b8137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "041f9770440a1660b78b",
+    "url": "/react-test/static/css/main.eac260af.chunk.css"
+  },
+  {
+    "revision": "041f9770440a1660b78b",
+    "url": "/react-test/static/js/main.041f9770.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/react-test/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "1661785bf39c40b732e1",
+    "url": "/react-test/static/js/2.1661785b.chunk.js"
+  },
+  {
+    "revision": "b2d9f38ec95f9d24c0be",
+    "url": "/react-test/static/js/runtime~main.b2d9f38e.js"
+  },
+  {
+    "revision": "5bd31a88ff373c6435d4c0d73a6ea27d",
+    "url": "/react-test/index.html"
+  }
+];
