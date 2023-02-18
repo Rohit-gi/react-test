37be5903efe67e65c4260d0eb1c149b8727290c9
Mohit <mokumar@forrester.com>
2023-02-18 23:39:47 +0530

    added chaanges

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -24,31 +24,31 @@ export default function TextForm(props) {
     props.showAlert("Text cleared!", "success");
   };
 
-  const handleCopy = ()=>{
+  const handleCopy = () => {
     console.log("I am copy");
     var text = document.getElementById("myBox");
     text.selec();
     text.setSelectionRange(0, 9999);
     navigator.clipboard.writeText(text.value);
     props.showAlert("Copied to Clipboard1", "success");
-  }
+  };
 
   const handleOnChange = (event) => {
     console.log("On change");
     setText(event.target.value);
   };
 
-  const handleExtraSpaces = ()=>{
+  const handleExtraSpaces = () => {
     let newText = text.split(/[]+/);
     setText(newText.join(""));
     props.showAlert("Extra spaces removed!", "success");
-  }
+  };
 
   return (
     <>
       <div
         className="container"
-        style={{color: props.mode === "dark" ? "white" : "#042743" }}
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
       >
         <h1>{props.heading}</h1>
         <div className="mb-3">
@@ -58,7 +58,7 @@ export default function TextForm(props) {
             onChange={handleOnChange}
             style={{
               backgroundColor: props.mode === "dark" ? "#13466e" : "white",
-              color: props.mode === "dark" ? "white" : "#042743"
+              color: props.mode === "dark" ? "white" : "#042743",
             }}
             id="myBox"
             rows="8"
@@ -70,24 +70,42 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>
           Convert to Lowercase
         </button>
-        <button className="btn btn-primary mx-2 my-1" onClick={handleclearClick}>
+        <button
+          className="btn btn-primary mx-2 my-1"
+          onClick={handleclearClick}
+        >
           Clear Text
         </button>
         <button className="btn btn-primary mx-2 my-1" onClick={handleCopy}>
           Copy Text
-        </button><button className="btn btn-primary mx-2 my-1" onClick={handleExtraSpaces}>
+        </button>
+        <button
+          className="btn btn-primary mx-2 my-1"
+          onClick={handleExtraSpaces}
+        >
           Remove Extra Spaces
         </button>
-        
       </div>
-      <div className="container my-3" style={{ color: props.mode === "dark" ? "white" : "#042743" }}>
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
         <h2>Your text summary</h2>
         <p>
-          {text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters
+          {
+            text.split(" ").filter((element) => {
+              return element.length !== 0;
+            }).length
+          }{" "}
+          words and {text.length} characters
         </p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
-        <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+        <p>
+          {text.length > 0
+            ? text
+            : "Enter something in the textbox above to preview it here"}
+        </p>
       </div>
     </>
   );
