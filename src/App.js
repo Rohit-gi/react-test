50c5eb26010b11bb39d7bed52a461d2b183442b3
Rohit-gi <i.rohit003@gmail.com>
2023-02-19 10:01:49 +0530

    node 14

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-// import About from "./components/About";
+import About from "./components/About";
 import React, { useState } from "react";
 import Alert from "./components/Alert";
 
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light"); //wheteher dark mode is enabled or not
@@ -35,25 +35,25 @@ function App() {
 
   return (
     <>
-      {/* <Router> */}
+      <Router>
         <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className="container my-3">
-          {/* <Switch> */}
-             {/* <Route exact path="/"> */}
+          <Switch>
+             <Route exact path="/">
               <TextForm
                 showAlert={showAlert}
                 heading="Enter the text to analyze below"
                 mode={mode}
               />
-            {/* </Route> */}
+            </Route>
 
-            {/* <Route exact path="/about">
-              <About /> */}
-            {/* </Route> */}
-          {/* </Switch> */}
+            <Route exact path="/about">
+              <About /> 
+             </Route>
+          </Switch>
         </div>
-     {/* </Router> */}
+     </Router>
     </>
   );
 }
